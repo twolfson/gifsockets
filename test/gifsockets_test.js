229f7ce3967a5496e9e61352e42c5c69572358ae
Todd Wolfson <todd@twolfson.com>
2013-11-18 02:29:25 -0800

    Correcting testing errors

--- test/gifsockets_test.js
+++ test/gifsockets_test.js
@@ -1,8 +1,21 @@
 var assert = require('assert');
+var Writable = require('stream').Writable;
 var imageUtils = require('./utils/image');
-var Gifsockets = require('../');
+var Gifsocket = require('../');
 
 describe('A connection to a gifsocket', function () {
+  before(function () {
+    this.gifsocket = new Gifsocket({
+      height: 10,
+      width: 10
+    });
+  });
+  before(function () {
+    this.stream = new Writable();
+    // this.streamData = this.stream.on('data'
+    this.gifsocket.addListener(this.stream);
+  });
+
   describe('writing an RGB frame', function () {
     it('receives a new frame', function () {
 
@@ -15,22 +28,22 @@ describe('A connection to a gifsocket', function () {
   });
 });
 
-describe.skip('A conection to a gifsocket', function () {
-  openImage();
-  describe('writing a first frame', function () {
-    imageUtils.load(__dirname + '/test-files/checkerboard.png');
-    drawJsonFrame();
+// describe.skip('A conection to a gifsocket', function () {
+//   openImage();
+//   describe.skip('writing a first frame', function () {
+//     imageUtils.load(__dirname + '/test-files/checkerboard.png');
+//     drawJsonFrame();
 
-    describe('and a second frame', function () {
-      imageUtils.load('checkerboard-inverse.png');
-      drawJsonFrame();
-      imageUtils.debug('multiple.png');
-      closeImage();
+//     describe('and a second frame', function () {
+//       imageUtils.load('checkerboard-inverse.png');
+//       drawJsonFrame();
+//       imageUtils.debug('multiple.png');
+//       closeImage();
 
-      it('receives both frames', function () {
-        var expectedImg = fs.readFileSync(__dirname + '/expected-files/multiple.gif', 'binary');
-        assert.strictEqual(this.gifData, expectedImg);
-      });
-    });
-  });
-});
+//       it('receives both frames', function () {
+//         var expectedImg = fs.readFileSync(__dirname + '/expected-files/multiple.gif', 'binary');
+//         assert.strictEqual(this.gifData, expectedImg);
+//       });
+//     });
+//   });
+// });
